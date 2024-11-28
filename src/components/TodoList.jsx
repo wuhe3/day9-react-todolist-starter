6db21ba76addcd67b163f4f957760d8ac5954442
wuhe3 <wuhe3@oocl.com>
2024-11-28 22:25:48 +0800

    feat, show msg if no item in todoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
 import TodoGroup from "./TodoGroup";
 import TodoGenerator from "./TodoGenerator"
+import {useContext} from 'react';
+import {TodoContext} from "../App";
 
 const TodoList = () => {
+    const {state: todoList} = useContext(TodoContext);
+    const description = "Add the things you need to do today..."
+
     return (
         <div>
+            <h1>TodoList</h1>
+            {todoList.length === 0 ? <p>{description}</p> : null}
             <TodoGroup></TodoGroup>
             <TodoGenerator></TodoGenerator>
         </div>
