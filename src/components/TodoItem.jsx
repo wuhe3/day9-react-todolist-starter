a3d75bca5f8903cdf05df22a2bb2cbd0846cf12f
WUHE3 <wuhe3@oocl.com>
2024-11-29 09:34:52 +0800

    extract, done, text, id

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,20 +5,21 @@ import './TodoItem.css';
 
 const TodoItem = (props) => {
     const {dispatch} = useContext(TodoContext);
+    const {done, text, id} = props.todoItem;
 
     const handleDelete = () => {
-        dispatch({type: "DELETE", payload: props.todoItem.id})
+        dispatch({type: "DELETE", payload: id})
     }
 
     const handleToggle = () => {
-        dispatch({type: "TOGGLE", payload: props.todoItem.id})
+        dispatch({type: "TOGGLE", payload: id})
     }
 
     return (
         <div className="todo-item-container">
             <span onClick={handleToggle} className="todo-item-wrapper"
-                  style={{textDecoration: props.todoItem.done ? 'line-through' : 'none'}}>
-                {props.todoItem.text}
+                  style={{textDecoration: done ? 'line-through' : 'none'}}>
+                {text}
             </span>
             <button onClick={handleDelete}>
                 x
