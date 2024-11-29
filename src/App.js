55a035242ae755ed337a4b4b201400d17b01e7bb
WUHE3 <wuhe3@oocl.com>
2024-11-29 10:42:43 +0800

    feat, implement router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import { createContext, useReducer } from "react";
+import {createContext, useReducer} from "react";
 import './App.css';
 import TodoList from "./components/TodoList";
-import { initialState, todoReducer } from "./context/todoReducer";
+import {initialState, todoReducer} from "./context/todoReducer";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import NotFoundPage from "./components/NotFoundPage";
 
 export const TodoContext = createContext();
 
 function App() {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+    const [state, dispatch] = useReducer(todoReducer, initialState);
 
-  return (
-    <div className="App">
-      <TodoContext.Provider value={{ state, dispatch }}>
-        <TodoList/>
-      </TodoContext.Provider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <TodoContext.Provider value={{state, dispatch}}>
+                <Router>
+                    <Routes>
+                        <Route path={"/"} element={<Navigate to={"/todo-list"}/>}/>
+                        <Route path={"/todo-list"} element={<TodoList/>}/>
+                        <Route path={"*"} element={<NotFoundPage/>}/>
+                    </Routes>
+                </Router>
+            </TodoContext.Provider>
+        </div>
+    );
 }
 
 export default App;
